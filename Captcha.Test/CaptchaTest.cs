d99a2a76f99ed69624b6ce09657e6c57d2c028d6
Wirachai <wirachai@iconext.co.th>
2015-11-24 16:26:22 +0700

    Create project with Unit Test

---
+++ Captcha.Test/CaptchaTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Captcha.Test
+{
+    [TestFixture]
+    class CaptchaTest
+    {
+        [Test]
+        public void Get_ShouldReturnOnePlus1_WhenInput1111()
+        {
+            Captcha captcha = new Captcha(1, 1, 1, 1);
+            var result = captcha.Get();
+            Assert.AreEqual("One + 1", result);
+        }
+    }
+}
---
+++ Captcha/Captcha.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Captcha
+{
+    public class Captcha
+    {
+        private int pattern;
+        private int leftOperand;
+        private int oper;
+        private int rightOperand;
+
+        public Captcha(int pattern, int left, int oper, int right)
+        {
+            this.pattern = pattern;
+            this.leftOperand = left;
+            this.oper = oper;
+            this.rightOperand = right;
+        }
+
+        public string Get()
+        {
+            return "One + 1";
+        }
+    }
+}
